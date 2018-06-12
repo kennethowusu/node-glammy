0b604b1dbe6eece7afc6d0808bfb2a6e89de8e96
Kenneth Owusu <keninchrist4eva@gmail.com>
2018-06-12 13:41:12 +0000

    commit

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+
+module.exports = function(grunt){
+
+  grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
+
+  //sass task
+  sass: {
+    dist: {
+      options: {
+        style: 'expanded'
+      },
+      files: {
+        'public/stylesheets/style.css': 'build/sass/main.scss'
+        //'widgets.css': 'widgets.scss'
+      }
+    }
+  },
+
+  //autoprefixer task
+  autoprefixer:{
+    options:{
+        // We need to `freeze` browsers versions for testing purposes.
+        browsers: ['opera 12', 'ff 15', 'chrome 25']
+    },
+
+     main_css: {
+      src: 'public/stylesheets/style.css',
+      dest: 'public/stylesheets/style.css'
+    }
+  },
+  // Concat fiiles
+ concat: {
+   options: {
+     // separator: ';',
+   },
+   dist: {
+     src: ['build/coffee/script.coffee', 'build/coffee/some.coffee', 'build/coffee/another.coffee'],
+     dest: 'build/coffee/main.coffee',
+   },
+ },
+
+  //process coffeescript to js
+  coffee:{
+    compile:{
+      files:{
+        'public/javascripts/main.js':'build/coffee/main.coffee'
+      }
+    }
+  },
+  //watch tasks
+  watch: {
+    options:{
+      livereload:false
+    },
+    scss: {
+      files: ['build/sass/**/*'],
+      tasks: ['sass','autoprefixer']
+    },
+    coffee:{
+      files:['build/coffee/**/*'],
+      tasks:['concat','coffee']
+    }
+
+  }
+});
+
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-autoprefixer');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-coffee');
+grunt.loadNpmTasks('grunt-contrib-concat');
+
+grunt.registerTask('default', ['watch']);
+grunt.registerTask('call_sass', 'sass');
+}
